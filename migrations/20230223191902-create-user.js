f4a8a3da74ad3d59dd17744e373252e6fdb04d9e
Adam Jankanič <xjankanic@stuba.sk>
2023-05-10 01:35:00 +0200

    Update 20230223191902-create-user.js

--- migrations/20230223191902-create-user.js
+++ migrations/20230223191902-create-user.js
@@ -36,7 +36,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE,
       },
-      updatedAt: {
+      updatedat: {
         allowNull: false,
         type: Sequelize.DATE,
       },

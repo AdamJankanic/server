995160dc56ae277d6b22b25af4eca2683376499f
Adam Jankanič <xjankanic@stuba.sk>
2023-05-10 00:45:35 +0200

    comment routes

--- route.js
+++ route.js
@@ -33,52 +33,43 @@ const { createCategory } = require("./controllers/categoryController");
 
 const { checkEmailDupe, checkDomain, checkToken } = require("./middlewares");
 
-// router.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
-
 /* HTTP requests */
 
 /*user*/
-router.post("/user/create", checkDomain, checkEmailDupe, createUser);
-router.post("/user/signup", signup);
-router.post("/user/verify", verify);
-router.post("/user/login", login);
-router.get("/user/logout/:uuid", logout);
-router.get("/user/refresh", refreshToken);
+// router.post("/user/create", checkDomain, checkEmailDupe, createUser);
+// router.post("/user/signup", signup);
+// router.post("/user/verify", verify);
+// router.post("/user/login", login);
+// router.get("/user/logout/:uuid", logout);
+// router.get("/user/refresh", refreshToken);
 
-router.get("/testik", (req, res) => {
-  res.send("testik");
-});
+// router.get("/testik", (req, res) => {
+//   res.send("testik");
+// });
 
-/*chat*/
-// router.post("/chat/create", createChat);
-router.get("/chat/mychats/:uuid", checkToken, getAllChatsByUser);
-router.get("/chat/messages/:uuid", getAllMessagesByChat);
-// router.get("/chat/mychats/:uuid", getAllChatsByUser);
+// /*chat*/
+// // router.post("/chat/create", createChat);
+// router.get("/chat/mychats/:uuid", checkToken, getAllChatsByUser);
+// router.get("/chat/messages/:uuid", getAllMessagesByChat);
+// // router.get("/chat/mychats/:uuid", getAllChatsByUser);
 
-/*event*/
-router.post("/event/create", checkToken, createEvent);
-router.put("/event/update", checkToken, updateEvent);
-router.get("/event/all", checkToken, getAllEvents);
-// get all events by user in url
-router.get("/event/myevents/:uuid", checkToken, getAllEventsByUser);
-// router.get("/event/myevents", getAllEventsByUser);
-router.post("/event/join", checkToken, joinEvent);
+// /*event*/
+// router.post("/event/create", checkToken, createEvent);
+// router.put("/event/update", checkToken, updateEvent);
+// router.get("/event/all", checkToken, getAllEvents);
+// // get all events by user in url
+// router.get("/event/myevents/:uuid", checkToken, getAllEventsByUser);
+// // router.get("/event/myevents", getAllEventsByUser);
+// router.post("/event/join", checkToken, joinEvent);
 
-/*offer*/
-router.get("/offer/all", checkToken, getAllOffers);
-router.put("/offer/update", checkToken, updateOffer);
-router.post("/offer/create", checkToken, createOffer);
-router.get("/offer/myoffers/:uuid", checkToken, getAllOffersByUser);
-router.post("/offer/contact", checkToken, contactSeller);
+// /*offer*/
+// router.get("/offer/all", checkToken, getAllOffers);
+// router.put("/offer/update", checkToken, updateOffer);
+// router.post("/offer/create", checkToken, createOffer);
+// router.get("/offer/myoffers/:uuid", checkToken, getAllOffersByUser);
+// router.post("/offer/contact", checkToken, contactSeller);
 
-/*category*/
-router.post("/category/create", createCategory);
+// /*category*/
+// router.post("/category/create", createCategory);
 
 module.exports = router;

82f39cf200604b48ca5c895b44fa57780ce0f8ea
Adam Jankanič <xjankanic@stuba.sk>
2023-05-09 20:42:56 +0200

    Update db.config.js

--- db.config.js
+++ db.config.js
@@ -1,35 +1,35 @@
 const { Sequelize, Model, DataTypes } = require("sequelize");
-const logger = require('../logger/api.logger');
+import mysql2 from "mysql2"; // Needed to fix sequelize issues with WebPack
+const logger = require("../logger/api.logger");
 
 const connect = () => {
+  const hostName = process.env.HOST;
+  const userName = process.env.USER;
+  const password = process.env.PASSWORD;
+  const database = process.env.DB;
+  const dialect = process.env.DIALECT;
 
-    const hostName = process.env.HOST;
-    const userName = process.env.USER;
-    const password = process.env.PASSWORD;
-    const database = process.env.DB;
-    const dialect = process.env.DIALECT;
+  const sequelize = new Sequelize(database, userName, password, {
+    host: hostName,
+    dialect: "mysql",
+    dialectModule: mysql2,
+    operatorsAliases: false,
+    pool: {
+      max: 10,
+      min: 0,
+      acquire: 20000,
+      idle: 5000,
+    },
+  });
 
-    const sequelize = new Sequelize(database, userName, password, {
-        host: hostName,
-        dialect: dialect,
-        operatorsAliases: false,
-        pool: {
-            max: 10,
-            min: 0,
-            acquire: 20000,
-            idle: 5000
-        }
-    });
+  const db = {};
+  db.Sequelize = Sequelize;
+  db.sequelize = sequelize;
+  db.tasks = require("../model/task.model")(sequelize, DataTypes, Model);
 
-    const db = {};
-    db.Sequelize = Sequelize;
-    db.sequelize = sequelize;
-    db.tasks = require("../model/task.model")(sequelize, DataTypes, Model);
-
-    return db;
-
-}
+  return db;
+};
 
 module.exports = {
-    connect
-}
+  connect,
+};

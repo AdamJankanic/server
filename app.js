9abd26cd3b6175f104d86dc534bb4a004b1d4c9a
Adam Jankanič <xjankanic@stuba.sk>
2023-05-10 02:21:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(express.static(path.join(__dirname, "images")));
 
 app.use(bodyParser.json());
 
-app.use("/", (req, res) => {
+app.use("/test", (req, res) => {
   res.send("Hello World!");
 });
 
